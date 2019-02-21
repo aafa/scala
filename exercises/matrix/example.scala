0ea413c21137a355743deb69b979de7418a8144d
Alexey Afanasev <avafanasiev@gmail.com>
2019-02-21 15:21:57 +0300

    remove all examples

--- exercises/matrix/example.scala
+++
@@ -1,9 +0,0 @@
-case class Matrix(matrix: String) {
-  lazy val rows: Vector[Vector[Int]] = matrix.lines.map(_.split(' ').map(_.toInt).toVector).toVector
-
-  lazy val columns: Vector[Vector[Int]] = rows.transpose
-
-  def row(i: Int) = rows(i)
-
-  def column(i: Int) = columns(i)
-}
--- exercises/word-count/example.scala
+++
@@ -1,8 +0,0 @@
-case class WordCount(phrase: String) {
-  def countWords = "\\w+('\\w+)*".r
-    .findAllIn(phrase)
-    .toSeq
-    .map(_.toLowerCase)
-    .groupBy(w => w)
-    .mapValues(_.length)
-}
